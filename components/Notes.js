2d601b86142aa992197f5303c21629fb99001917
cuervo357@gmail.com <cuervo357@gmail.com>
2021-08-15 11:10:24 -0500

    update

--- components/Notes.js
+++ components/Notes.js
@@ -2,13 +2,12 @@ import React, {useState, useEffect} from 'react'
 import { connect } from 'react-redux'
 import { StyleSheet, View, Text, ScrollView, TextInput, Button, Alert } from 'react-native'
 import { MarkdownView } from 'react-native-markdown-view'
-import {deleteNote, getNotes, postNote, putNote} from '../redux/actions'
+import {deleteNote, getNoteId, getNotes, postNote, putNote} from '../redux/actions'
 
-function Notes({getNotes, putNote, deleteNote, postNote, notes}) {
+function Notes({getNotes, getNoteId, putNote, deleteNote, postNote, notes, note}) {
   const [saveDisabled, setSaveDisabled] = useState(true)
-  const [currentId, setCurrentId] = useState('')
   const [updateNote, setUpdateNote] = useState({note: ''})
-  const [showContent, setShowContent] = useState({})
+  const [showContent, setShowContent] = useState(false)
   const [create, setCreate] = useState('')
   const [showCreate, setShowCreate] = useState(false)
 
@@ -16,10 +15,9 @@ function Notes({getNotes, putNote, deleteNote, postNote, notes}) {
     getNotes()
   }, [])  
 
-  function handleEdit(_id, note) {
+  function handleEdit() {
     setSaveDisabled(!saveDisabled)
-    setUpdateNote({...updateNote, note: note})
-    setCurrentId(_id)
+    setUpdateNote({...updateNote, note: note && note.note})
   }
 
   function handleChange(e) {
@@ -27,12 +25,12 @@ function Notes({getNotes, putNote, deleteNote, postNote, notes}) {
   }
 
   async function handleSave() {
-    await putNote(currentId, updateNote)
-    getNotes()
+    await putNote(note && note._id, updateNote)
+    getNoteId(note && note._id)
     setSaveDisabled(!saveDisabled)
   }
 
-  async function handleDelete(id) {
+  async function handleDelete() {
     Alert.alert(
       "Delete note",
       "Are yoy sure?",
@@ -42,18 +40,19 @@ function Notes({getNotes, putNote, deleteNote, postNote, notes}) {
           onPress: () => console.log("Cancel Pressed"),
         },
         { text: "OK", onPress: async () => {
-          await deleteNote(id)
+          await deleteNote(note && note._id)
+          setShowContent(false)
           getNotes()
-        } }
+        }}
       ]
     );
   }
 
-  function handleShow(_id) {
-    setShowContent(note => ({
-      ...note,
-      [_id]: !note[_id]
-    }))
+  async function handleShow(_id) {
+    await getNoteId(_id)
+    setShowContent(true)
+    setUpdateNote({note: ''})
+    setSaveDisabled(true)
   }
 
   async function handleCreate() {
@@ -72,46 +71,56 @@ function Notes({getNotes, putNote, deleteNote, postNote, notes}) {
               <MarkdownView >{el.note.split('').splice(0,20)}</MarkdownView>
             </Text>
           ) : null}
-          {showContent[el._id] ? 
-          <View style={styles.noteContent}>
-            <View style={styles.readBox}>
-              <MarkdownView>#{el.note.split('').splice(0,20).join('')}</MarkdownView>
-              <MarkdownView>{el.note}</MarkdownView>
-            </View>
-            <View style={styles.writeBox}>
-              <TextInput
-                value={updateNote.note}
-                numberOfLines={5}
-                multiline
-                editable={!saveDisabled}
-                textAlignVertical={'top'}
-                onChangeText={(e) => handleChange(e)} 
-              />
-            </View>
-            <View>
-              <Text>Created on {new Date(el.createdAt).getDate()}/{new Date(el.createdAt).getMonth()+1}/{new Date(el.createdAt).getFullYear()}</Text>
-              <Text>Contains {el.note.length} words</Text>
-              </View>
-            <View style={{flex:1, flexDirection:'row', justifyContent: 'space-around', marginTop:10}}>
-              <Button title='Edit' onPress={(_id, note) => handleEdit(el._id, el.note)} />
-              <Button title='Save' disabled={saveDisabled} onPress={handleSave} />
-              <Button title='Delete' color='firebrick' onPress={() => handleDelete(el._id)} />
-              <Button title='Close' color='slategray' onPress={() => handleShow(el._id) } />
-            </View>
-          </View > : null}
-        </View>  
+        </View> 
       )}
-      <View style={{marginBottom: 50}}>
+
+      <View style={showContent ? styles.noteContent : styles.noteContentHidden}>
+        <View>
+          <View style={styles.readBox}>
+            <MarkdownView>#{note && note.note.split('').splice(0,20).join('')}</MarkdownView>
+            <MarkdownView>{note && note.note}</MarkdownView>
+          </View>
+
+          <View style={styles.writeBox}>
+            <TextInput 
+              value={updateNote.note}
+              numberOfLines={5}
+              multiline
+              editable={!saveDisabled}
+              textAlignVertical={'top'}
+              onChangeText={(e) => handleChange(e)} 
+            />
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>Created on {new Date(note && note.createdAt).getDate()}/{new Date(note && note.createdAt).getMonth()+1}/{new Date(note && note.createdAt).getFullYear()}</Text>
+            <Text>Contains {note && note.note.length} words</Text>
+          </View>
+
+          <View style={{flex:1, flexDirection:'row', justifyContent: 'space-around', marginTop:10}}>
+            <Button title='Edit' onPress={handleEdit}/>
+            <Button title='Save' disabled={saveDisabled} onPress={handleSave} />
+            <Button title='Delete' color='firebrick' onPress={handleDelete} />
+            <Button title='Close' color='slategray' onPress={() => setShowContent(false) } />
+          </View>
+        </View>
+      </View>
+      
+      <View style={{marginBottom: 50, marginTop:20}}>
         <Button onPress={handleCreate} color='limegreen' title='Create note'/>
         <TextInput 
-        style={showCreate ? {borderWidth: 1, borderStyle: 'solid', borderColor:'black'}: {display:'none'}}
-        value={create}
-        numberOfLines={5}
-        multiline
-        editable
-        textAlignVertical={'top'}
-        onChangeText={(text) => setCreate(text)} />
+          style={showCreate ? {borderWidth: 1, borderStyle: 'solid', borderColor:'black'}: {display:'none'}}
+          value={create}
+          numberOfLines={5}
+          multiline
+          editable
+          textAlignVertical={'top'}
+          onChangeText={(text) => setCreate(text)} 
+        />
       </View>
+
     </ScrollView>
     
   )
@@ -119,13 +128,15 @@ function Notes({getNotes, putNote, deleteNote, postNote, notes}) {
 
 function mapStateToProps(state) {
   return {
-    notes: state.notes
+    notes: state.notes,
+    note: state.note
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     getNotes: () => dispatch(getNotes()),
+    getNoteId: (id) => dispatch(getNoteId(id)),
     putNote: (id, payload) => dispatch(putNote(id, payload)),
     deleteNote: (id) => dispatch(deleteNote(id)),
     postNote: (data) => dispatch(postNote(data))
@@ -148,6 +159,9 @@ const styles = StyleSheet.create({
   noteContent: {
     padding: 5,
     margin: 5,
+  },
+  noteContentHidden: {
+    display: 'none',
   },
   readBox: {
     padding: 5
